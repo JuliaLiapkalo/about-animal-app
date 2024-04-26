600899bb609eb59a4677dc76cd673cae7eacf8ce
Julia <julialyapkalo@gmail.com>
2024-04-26 20:41:06 +0300

    added repository

---
+++ src/main/java/com/liapkalo/pumb/aboutanimal/repository/AnimalRepository.java
@@ -0,0 +1,15 @@
+package com.liapkalo.pumb.aboutanimal.repository;
+
+import com.liapkalo.pumb.aboutanimal.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    @Query(value = "select id from animal", nativeQuery = true)
+    List<Long> findAllAnimalIds();
+}
