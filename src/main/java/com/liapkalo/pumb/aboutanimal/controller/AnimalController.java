b7436fd258a3d7407e52eeb54747d8fa505c2164
Julia <julialyapkalo@gmail.com>
2024-04-26 20:45:47 +0300

    added controller

---
+++ src/main/java/com/liapkalo/pumb/aboutanimal/controller/AnimalController.java
@@ -0,0 +1,40 @@
+package com.liapkalo.pumb.aboutanimal.controller;
+
+import com.liapkalo.pumb.aboutanimal.factory.ReadFileFactory;
+import com.liapkalo.pumb.aboutanimal.service.AnimalService;
+import jakarta.xml.bind.JAXBException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/animals")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AnimalController {
+
+    ReadFileFactory readFileFactory;
+    AnimalService animalService;
+
+    @PostMapping("/uploads")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> processAnimalFile(@RequestParam("file") MultipartFile file) throws JAXBException, FileNotFoundException {
+        readFileFactory.getFileReader(file).readFile(file);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getFiltersAnimals(@RequestParam(required = false) String type,
+                                               @RequestParam(required = false) String category,
+                                               @RequestParam(required = false) String gender,
+                                               @RequestParam(required = false) String sortBy
+    ) {
+        return ResponseEntity.ok().body(animalService.getFilteredAnimals(type, category, gender, sortBy));
+    }
+}
