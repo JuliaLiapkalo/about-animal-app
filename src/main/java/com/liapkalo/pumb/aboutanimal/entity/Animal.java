c267d78d41eb4c290c50e20b5ccc423fce55c714
Julia <julialyapkalo@gmail.com>
2024-04-26 20:40:30 +0300

    added entities

---
+++ src/main/java/com/liapkalo/pumb/aboutanimal/entity/Animal.java
@@ -0,0 +1,37 @@
+package com.liapkalo.pumb.aboutanimal.entity;
+
+import com.liapkalo.pumb.aboutanimal.enums.Sex;
+import com.liapkalo.pumb.aboutanimal.enums.Type;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@ToString
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    @Enumerated(EnumType.STRING)
+    Type type;
+
+    @Enumerated(EnumType.STRING)
+    Sex sex;
+
+    int weight;
+
+    int cost;
+
+    int category;
+
+}
