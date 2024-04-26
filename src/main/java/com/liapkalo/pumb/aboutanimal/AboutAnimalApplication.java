831f16011f35f0c8d5d9712275a91db0d212bead
Julia <julialyapkalo@gmail.com>
2024-04-26 20:45:58 +0300

    added entry point

---
+++ src/main/java/com/liapkalo/pumb/aboutanimal/AboutAnimalApplication.java
@@ -0,0 +1,13 @@
+package com.liapkalo.pumb.aboutanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutAnimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboutAnimalApplication.class, args);
+	}
+
+}
