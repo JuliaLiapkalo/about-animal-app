234f926c8a1f12c02b32079bca3f1db5ef248bcc
Julia <julialyapkalo@gmail.com>
2024-04-28 18:52:58 +0300

    refactoring

--- src/main/java/com/liapkalo/pumb/aboutanimal/service/impl/ReadCsvFileServiceImpl.java
+++ src/main/java/com/liapkalo/pumb/aboutanimal/service/impl/ReadCsvFileServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import static com.liapkalo.pumb.aboutanimal.utils.BuildUtils.buildAnimalDto;
 
@@ -24,16 +25,19 @@ public class ReadCsvFileServiceImpl implements ReadFileService {
     @Override
     public void readFile(MultipartFile file) {
         try (CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()))) {
-            String[] csvHeader = reader.readNext();
-            if (isCvsValid(csvHeader)) {
-                String[] nextLine;
-                while ((nextLine = reader.readNext()) != null) {
-                    createAnimal(nextLine);
-                }
+            if (!isCvsValid(reader.readNext())) {
+                log.error("Invalid CSV format");
+                return;
             }
-            } catch(IOException | CsvValidationException e){
-                log.error("Error reading CSV file", e);
+
+            String[] nextLine;
+            while ((nextLine = reader.readNext()) != null) {
+                createAnimal(nextLine);
             }
+
+        } catch (IOException | CsvValidationException e) {
+            log.error("Error reading CSV file", e);
+        }
     }
 
     private void createAnimal(String[] nextLine) {
@@ -44,10 +48,10 @@ public class ReadCsvFileServiceImpl implements ReadFileService {
 
     private boolean isCvsValid(String[] csvHeader) {
         return csvHeader[0].equalsIgnoreCase("name") &&
-                csvHeader[1].equalsIgnoreCase("type") &&
-                csvHeader[2].equalsIgnoreCase("sex") &&
-                csvHeader[3].equalsIgnoreCase("weight") &&
-                csvHeader[4].equalsIgnoreCase("cost");
+               csvHeader[1].equalsIgnoreCase("type") &&
+               csvHeader[2].equalsIgnoreCase("sex") &&
+               csvHeader[3].equalsIgnoreCase("weight") &&
+               csvHeader[4].equalsIgnoreCase("cost");
     }
 
     private boolean isLineValid(String[] nextLine) {
@@ -55,12 +59,7 @@ public class ReadCsvFileServiceImpl implements ReadFileService {
     }
 
     private boolean isObjectFull(String[] array) {
-        for (String str : array) {
-            if (str.isEmpty()) {
-                return false;
-            }
-        }
-        return true;
+       return Arrays.stream(array).noneMatch(String::isEmpty);
     }
 }
 
